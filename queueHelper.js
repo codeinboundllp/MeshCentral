6d1ff8241d215c2ebf70fd46435def45ec0ca9d6
Sushil Kumar Gupta <kalwarsushil2@gmail.com>
2025-02-18 18:22:01 +0530

    queue connetion fix

--- queueHelper.js
+++ queueHelper.js
@@ -1,20 +1,30 @@
 const Queue = require('bull');
 
-const myQueue = new Queue('CommunicationQueue', {
-    redis: { host: '127.0.0.1', port: 6380 } // Adjust Redis connection settings as needed
-});
+
 
 // Function to add a job to the queue
 async function publishQueueJob(sessionId, status) {
+    // Connect to Redis
+    const myQueue = new Queue('CommunicationQueue', {
+        redis: { host: '127.0.0.1', port: 6380 } // Adjust Redis connection settings as needed
+    });
     if(sessionId && status){
         await myQueue.add("Update_Session_Status", { session_id: sessionId, status: status });
     }
+    // Disconnect from Redis
+    await myQueue.client.quit(); // Disconnect from Redis
 }
 
 // Function to add a job to the queue and get data
 async function getDataQueueJob(sessionid) {
+    // Connect to Redis
+    const myQueue = new Queue('CommunicationQueue', {
+        redis: { host: '127.0.0.1', port: 6380 } // Adjust Redis connection settings as needed
+    });
     const job = await myQueue.add("Get_Session_Details", sessionid);
     const data = await job.finished();
+    // Disconnect from Redis
+    await myQueue.client.quit(); // Disconnect from Redis
     return data;
 }
 
